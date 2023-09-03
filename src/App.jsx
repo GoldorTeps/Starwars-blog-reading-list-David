a29bf682bd8d6f2a1a3a4e15488f9c4eff80144d
Goldor Morigui <agolfofree@hotmail.com>
2023-09-03 22:53:09 +0200

    falta la mitad

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'; 
+import React from 'react';
 import useAppContext from './context/appContext';
 import Card from './components/card';
 import Navbar from './components/navbar';
@@ -12,42 +12,43 @@ function App() {
 
   return (
     <>
-   
-    <Navbar />
-    
-    <div className='cardContainer'>
-      {store.people.map((person) => (
-        <Card
-          img={`https://starwars-visualguide.com/assets/img/characters/${person.result.uid}.jpg`}
-          title={person.result.properties.name}
-          key={person.result.uid}
-       
-        />
-      ))}
-         </div>
-         <div className='cardContainer'>
-      {store.planets.map((planets) => (
-        <Card
-          img={`https://starwars-visualguide.com/assets/img/planets/${planets.result.uid}.jpg`}
-          title={planets.result.properties.name}
-          key={planets.result.uid}
-       
-        />
-      ))}
-         </div>
-         <div className='cardContainer'>
-      {store.vehicles.map((vehicles) => (
-        <Card
-          img={`https://starwars-visualguide.com/assets/img/vehicles/${vehicles.result.uid}.jpg`}
-          title={vehicles.result.properties.model}
-          key={vehicles.result.uid}
-       
-        />
-      ))}
-         </div>
+      <Navbar />
+
+      <div className='cardContainer'>
+        {store.people.map((person) => (
+          <Card
+            img={`https://starwars-visualguide.com/assets/img/characters/${person.result.uid}.jpg`}
+            title={person.result.properties.name}
+            key={person.result.uid}
+          />
+        ))}
+      </div>
+
+      <div className='cardContainer'>
+        {store.planets.map((planets) => (
+          <Card
+            img={`https://starwars-visualguide.com/assets/img/planets/${planets.result.uid}.jpg`}
+            title={planets.result.properties.name}
+            key={planets.result.uid}
+          />
+        ))}
+      </div>
+
+      <div className='cardContainer'>
+        {store.vehicles ? (
+          store.vehicles.map((vehicles) => (
+            <Card
+              img={`https://starwars-visualguide.com/assets/img/vehicles/${vehicles.result.uid}.jpg`}
+              title={vehicles.result.properties.model}
+              key={vehicles.result.uid}
+            />
+          ))
+        ) : (
+          <p>Loading...</p>
+        )}
+      </div>
     </>
   );
-}; 
-
-export default App;
+        }; 
+  export default App;
 
