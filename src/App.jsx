da5a338287915ffe5ae149923cdd815afd646d94
Goldor Morigui <agolfofree@hotmail.com>
2023-09-03 23:49:27 +0200

    rutas casi

--- src/App.jsx
+++ src/App.jsx
@@ -20,6 +20,7 @@ function App() {
             img={`https://starwars-visualguide.com/assets/img/characters/${person.result.uid}.jpg`}
             title={person.result.properties.name}
             key={person.result.uid}
+            to= {`/people/${person.result.uid}`}
           />
         ))}
       </div>
@@ -30,6 +31,7 @@ function App() {
             img={`https://starwars-visualguide.com/assets/img/planets/${planets.result.uid}.jpg`}
             title={planets.result.properties.name}
             key={planets.result.uid}
+            to= {`/planets/${planets.result.uid}`}
           />
         ))}
       </div>
@@ -41,6 +43,7 @@ function App() {
               img={`https://starwars-visualguide.com/assets/img/vehicles/${vehicles.result.uid}.jpg`}
               title={vehicles.result.properties.model}
               key={vehicles.result.uid}
+              to= {`/vehicles/${vehicles.result.uid}`}
             />
           ))
         ) : (
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import { BrowserRouter } from 'react-router-dom';
 import './index.css'
 import { AppProvider } from './context/appContext.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
   <AppProvider>
     <App />
-  </AppProvider>,
+  </AppProvider>
+  </BrowserRouter>,
 )
